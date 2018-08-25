e414540ea71aae62db2bc0ca1bbbf5838653c34a
Michael Beswetherick <mbeswetherick@gmail.com>
2018-08-25 19:58:39 -0400

    infinite notes

--- harmony/harmony.js
+++ harmony/harmony.js
@@ -1,24 +1,29 @@
+const handleNoteClick = function () {
+  const keyNumber = this.id;
+  const key = document.getElementById(`key${keyNumber}`);
+  if (key.paused) {
+    key.play();
+  } else {
+    key.pause();
+  }
+};
+
+const KEY_NUMS = [1, 2, 3, 4, 5, 6, 7];
 
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   //do work
-  document.getElementById('1').onclick = () => {
-    const key = document.getElementById('key1');
-    if (key.paused) {
-      key.play();
-    } else {
-      key.pause();
-    }
 
-  };
+  KEY_NUMS.forEach(keyNum => document.getElementById(keyNum).onclick = handleNoteClick);
+  // document.getElementById('1').onclick = handleNoteClick;
 
-  document.getElementById('2').onclick = () => {
-    const key = document.getElementById('key2');
-    if (key.paused) {
-      key.play();
-    } else {
-      key.pause();
-    }
+  // document.getElementById('2').onclick = function () {
+  //   const key = document.getElementById('key2');
+  //   if (key.paused) {
+  //     key.play();
+  //   } else {
+  //     key.pause();
+  //   }
 
-  };
+  // };
 });
