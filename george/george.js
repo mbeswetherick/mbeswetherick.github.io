7fc6e7eb58b47b50770ae3bc2dba68d8aca7d93c
Michael Beswetherick <mbeswetherick@gmail.com>
2018-08-30 22:11:34 -0400

    all sorts of stuff

--- george/george.js
+++ george/george.js
@@ -5,12 +5,12 @@ const PHOTOS = [
     background: 'white', 
   }, 
   {
-    image: 'photos/07120027.JPG',
+    image: 'photos/closeup.JPG',
     opacity: 0,
     background: '#121212',
   },
   {
-    image: 'photos/07120027.JPG',
+    image: 'photos/closeup.JPG',
     opacity: 1,
     background: 'white',
   },
@@ -18,7 +18,13 @@ const PHOTOS = [
     image: 'photos/07120019.JPG',
     opacity: 1,
     background: 'white',
+    textColor: 'black-text',
     class: 'tiny-frame',
+  },
+  {
+    image: 'photos/churchSky.JPG',
+    opacity: 1,
+    background: 'white',
   },
 ]
 
@@ -33,10 +39,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     entries.forEach(entry => {
       if (entry.intersectionRatio > 0) {
         const slide = PHOTOS[parseInt(entry.target.id, 10)];
+        const textDom = document.getElementById(entry.target.id);
         console.log('in the view', entry);
         //entry.target.style.color = 'pink';
         coverImage.style.opacity = 0;
         body.style.background = slide.background;
+        if (slide.textColor) {
+          textDom.classList.add(slide.textColor);
+        } else {
+          textDom.classList.add(slide.textColor);
+        }
         if (slide.class) {
           holder.classList.add(slide.class);
         } else {
